31ee210f2d9bacdc0e116fbea262daa87be566dd
andrew <>
2025-03-20 23:00:34 +0900

    PWA 시작

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,17 @@ import { ToastContainer } from "react-toastify";
 
 import NavBar from "./components/Navbar/Navbar";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { pwaTrackingListeners } from "./scripts/pwaEventlisteners";
 
 import "./App.scss";
 import "react-toastify/dist/ReactToastify.css";
+
+const isBrowser = typeof window !== "undefined";
+
+if (isBrowser) {
+  pwaTrackingListeners();
+}
+
 
 function App() {
   return (
@@ -28,3 +36,16 @@ function App() {
 }
 
 export default App;
+
+if (isBrowser && "serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => {
+        console.log("Service worker registered");
+      })
+      .catch((err) => {
+        console.log("Service worker registration failed", err);
+      });
+  });
+}
